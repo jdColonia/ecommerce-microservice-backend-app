049b7e624cead3c2291851951617ec4136c5f522
jdColonia <JuanDavidColoniaAldana@gmail.com>
2025-06-11 18:31:36 -0500

    fix: password sql

--- user-service/src/main/resources/db/migration/V6__insert_credentials_table.sql
+++ user-service/src/main/resources/db/migration/V6__insert_credentials_table.sql
@@ -9,28 +9,28 @@ INSERT INTO
 VALUES (
         1,
         'selimhorri',
-        '$2a$04$/S7cWjHPZul03sPEivycWeKTBvLyjYdaRWmeaFbiqKy9es/3W4QB6',
+        '$2a$10$LK9Oiyv1vw3fIAHDrRGdXuIfizqoov6xGfq7QQFG1xzGyXwEy0z8u',
         'ROLE_USER',
         TRUE
     ),
     (
         2,
         'amineladjimi',
-        '$2a$04$8D8OuqPbE4LhRckvtBAHrOmpeWmE92xNNVtyK8Z/lrJFjsImpjBkm',
+        '$2a$10$LK9Oiyv1vw3fIAHDrRGdXuIfizqoov6xGfq7QQFG1xzGyXwEy0z8u',
         'ROLE_USER',
         TRUE
     ),
     (
         3,
         'omarderouiche',
-        '$2a$04$jelNGcF4wFHJirT5Pm7jPO8812QE/3tIWIs1DNnajS68iG4aKUqvS',
+        '$2a$10$LK9Oiyv1vw3fIAHDrRGdXuIfizqoov6xGfq7QQFG1xzGyXwEy0z8u',
         'ROLE_USER',
         TRUE
     ),
     (
         4,
         'admin',
-        '$2a$04$1G4TwSzwf5JwZ4dKCXG1Zu1Qh3WIY9JNaM9vF6Ff05QDfyPg7nSxO',
+        '$2a$10$LK9Oiyv1vw3fIAHDrRGdXuIfizqoov6xGfq7QQFG1xzGyXwEy0z8u',
         'ROLE_USER',
         TRUE
     );

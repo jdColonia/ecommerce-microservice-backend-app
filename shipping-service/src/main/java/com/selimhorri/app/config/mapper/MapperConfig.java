23e18c4c3db75b3bc6257087d3abfd55c8e4efb0
jdColonia <JuanDavidColoniaAldana@gmail.com>
2025-06-11 01:36:04 -0500

    fix: fix MapperConfig of all microservices

--- shipping-service/src/main/java/com/selimhorri/app/config/mapper/MapperConfig.java
+++ shipping-service/src/main/java/com/selimhorri/app/config/mapper/MapperConfig.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 public class MapperConfig {
 
     @Bean
-    public ObjectMapper objectMapperBean() {
+    public ObjectMapper objectMapper() {
         return JsonMapper.builder()
                 .addModule(new JavaTimeModule())
                 .addModule(new ParameterNamesModule())
